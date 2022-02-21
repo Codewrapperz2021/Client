014670d35a4171fb7ef1aec1d7abc1ea1b1a8581
codewrapperz2021 <codewrapperz360@gmail.com>
2022-02-21 15:57:29 +0530

    add1

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
-// const sockets = io('https://server-3wl8.onrender.com', { autoConnect: true, forceNew: true });
-const sockets = io('https://master.d2p32jz8cu0did.amplifyapp.com', { autoConnect: true, forceNew: true });
+const sockets = io('https://server-3wl8.onrender.com', { autoConnect: true, forceNew: true });
+// const sockets = io('https://master.d2p32jz8cu0did.amplifyapp.com', { autoConnect: true, forceNew: true });
 export default sockets;
